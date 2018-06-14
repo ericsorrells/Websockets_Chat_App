129e66a1ca612cdd3d3eeb12ce47e915b8788339
Eric Sorrells <ericsorrells@hotmail.com>
2018-06-14 09:18:09 -0400

    working in basic state: sending/receiving messages

--- src/redux/middleware/middleware.js
+++ src/redux/middleware/middleware.js
@@ -8,44 +8,66 @@ const createSocketMiddleware = (
   shouldInstantiate,  // a predicate function to know when to connect our socket
   eventHandlers       // the actions we want our socket to dispatch
 ) => store => next => action => {
-  console.log('SHOULD_INSTANTIATE', shouldInstantiate(action))
-  console.log('ACTION', action)
-  const SOCKET_STATES = {
-    CONNECTING: 0,
-    OPEN: 1,
-    CLOSING: 2,
-    CLOSED: 3
-  };
+  // console.log('SHOULD_INSTANTIAVTE', shouldInstantiate(action))
+  // console.log('ACTION', action)
+  // const SOCKET_STATES = {
+  //   CONNECTING: 0,
+  //   OPEN: 1,
+  //   CLOSING: 2,
+  //   CLOSED: 3
+  // };
 
-  let ws = new WebSocket(socketURL)
+  let websocket = new WebSocket(socketURL);
+  websocket.onopen    = function (evt) { onOpen(evt)    };
+  websocket.onclose   = function (evt) { onClose(evt)   };
+  websocket.onmessage = function (evt) { onMessage(evt) };
+  websocket.onerror   = function (evt) { onError(evt)   };
 
-  if (shouldInstantiate(action)) {
-    // instantiate the web socket
-    // const ws = new window.WebSocket(socketURL)
-    console.log('WEBSOCKET INSTANTIATINGi: websocket', ws)
-    console.log('WEBSOCKET INSTANTIATINGi: readyState', ws.readyState)
-
-    // bind eventHandlers to dispatch
-    const boundEventHandlers = bindActionCreators(eventHandlers, store.dispatch)
-    // fire onopen event, and fire off a subscribe message with our handshake data
-    ws.onopen = e => {
-      boundEventHandlers.onopen(e)
-      ws.send(JSON.stringify({ type: 'subscribe', ...subscribeData }))
-    }
-    // assign remaining event handlers
-    ws.onclose = boundEventHandlers.onclose
-    ws.onerror = boundEventHandlers.onerror
-    ws.onmessage = boundEventHandlers.onmessage
-  } else {
-    console.log('INSIDE ELSE STMT: sending', action)
-    console.log('INSIDE ELSE STMT: socket', ws)
-    if (ws.readyState === SOCKET_STATES.OPEN) {
-      ws.send(action)
-    } else {
-      console.log('NOT IN READY STATE: ', ws.readyState)
-    }
-    return next(action)
+  function onOpen(evt) {
+    websocket.send(action);
   }
+
+  function onClose(evt) {
+    console.log("DISCONNECTED");
+  }
+
+  function onMessage(evt) {
+    console.log('RECEIVING MESSAGE!', evt)
+    // websocket.close();
+  }
+
+  function onError(evt) {
+    console.log('ON ERROR', evt)
+
+  }
+
+  // if (shouldInstantiate(action)) {
+  // instantiate the web socket
+  // const ws = new window.WebSocket(socketURL)
+  // console.log('WEBSOCKET INSTANTIATINGi: websocket', ws)
+  // console.log('WEBSOCKET INSTANTIATINGi: readyState', ws.readyState)
+
+  // bind eventHandlers to dispatch
+  // const boundEventHandlers = bindActionCreators(eventHandlers, store.dispatch)
+  // fire onopen event, and fire off a subscribe message with our handshake data
+  // ws.onopen = e => {
+  //   boundEventHandlers.onopen(e)
+  //   ws.send(JSON.stringify({ type: 'subscribe', ...subscribeData }))
+  // }
+  // assign remaining event handlers
+  //   ws.onclose = boundEventHandlers.onclose
+  //   ws.onerror = boundEventHandlers.onerror
+  //   ws.onmessage = boundEventHandlers.onmessage
+  // } else {
+  //   console.log('INSIDE ELSE STMT: sending', action)
+  //   console.log('INSIDE ELSE STMT: socket', ws)
+  //   if (ws.readyState === SOCKET_STATES.OPEN) {
+  //     ws.send(action)
+  //   } else {
+  //     console.log('NOT IN READY STATE: ', ws.readyState)
+  //   }
+  //   return next(action)
+  // }
 }
 
 
